01223cddc1796a0d3174b7ca81c3bf78838f1ea4
Idan Fischer <Idanfischer5@gmail.com>
2023-02-28 11:21:01 -0500

    added simple docker

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express"
+import cors from "cors"
+
+const PORT = process.env.PORT || 3030
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+app.get('/test', (req, res) => {
+  res.send(`🐳 Docker is cool.`)
+})
+
+app.listen(PORT, () => {
+  console.log(`Listening on http://localhost:3030...`)
+})
